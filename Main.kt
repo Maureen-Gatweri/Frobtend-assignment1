e1e187c6e6831c58510d3eb7d04bbe8c58798f51
Maureen <maureennmwendwa@gmail.com>
2024-02-06 23:12:12 +0300

    add 1 kotlin file

---
+++ Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var fullName = "Maureen Mwendwa"
+    println(fullName)
+    var age ="20"
+    println(age)
+    var phoneNumber = "0704264110"
+    println(phoneNumber)
+    var weight = "64.5kg"
+    println(weight)
+    var nationality = "Kenyan"
+    println(nationality)
+
+
+}
